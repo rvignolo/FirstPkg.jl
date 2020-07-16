59725e6762a44fbfaab4da59be2a55381b7ceea6
Rami Vignolo <ramirovignolo@gmail.com>
2020-07-15 23:18:57 -0300

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -7,7 +7,7 @@ makedocs(;
     repo="https://github.com/rvignolo/FirstPkg.jl/blob/{commit}{path}#L{line}",
     sitename="FirstPkg.jl",
     format=Documenter.HTML(;
-        prettyurls="false",
+        prettyurls=false,
         canonical="https://rvignolo.github.io/FirstPkg.jl",
         assets=String[],
     ),

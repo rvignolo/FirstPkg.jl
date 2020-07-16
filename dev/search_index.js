9aa2ba837c28d15d15d2065666c10b57a52b0a0a
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2020-07-16 13:05:39 +0000

    build based on b553fbe

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"index.html","page":"Home","title":"Home","text":"CurrentModule = FirstPkg","category":"page"},{"location":"index.html#FirstPkg","page":"Home","title":"FirstPkg","text":"","category":"section"},{"location":"index.html","page":"Home","title":"Home","text":"","category":"page"},{"location":"index.html","page":"Home","title":"Home","text":"Modules = [FirstPkg]","category":"page"}]
+}
